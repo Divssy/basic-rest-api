a3a63c0cb83784138e22015757e3a7d93b9d3e09
Divssy <divya_alfarina_32rpl@student.smktelkom-mlg.sch.id>
2024-10-03 10:56:53 +0700

    menambahkan file transaksi

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import  Express  from "express"
 import medicineRouter from "./router/medicineRouter"
 import adminRouter from "./router/adminRouter"
+import transactionRouter from "./router/transactionRouter"
 
 const app = Express()
 /** allow to read a body request with JSON format */
@@ -11,6 +12,8 @@ app.use(Express.json())
 app.use(`/medicine`, medicineRouter)
 
 app.use(`/admin`, adminRouter)
+
+app.use(`/transaction`, transactionRouter)
 
 const PORT = 1992
 app.listen(PORT, () => {
